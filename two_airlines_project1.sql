37c21cf55457a4b516104ff3274563c96899c5d1
Mickybat <99045830+Mickybat@users.noreply.github.com>
2023-02-08 23:16:10 -0500

    Update two_airlines_project1.sql

--- two_airlines_project1.sql
+++ two_airlines_project1.sql
@@ -1,40 +1,22 @@
 DROP TABLE IF EXISTS two_airlines;
 
 CREATE TABLE two_airlines ( 
-airline varchar(100),
-status varchar(100), 
-los_angeles int,
-phoenix int, 
-san_diego int, 
-san_francisco int, 
-seattle int
+City varchar(100),
+Alaska_ON_Time varchar(100), 
+Alaska_Delayed varchar(100),
+AM_WEST_ON_Time varchar(100), 
+AM_WEST_Delayed varchar(100)
 ); 
 
 INSERT INTO two_airlines 
-VALUES ('ALASKA', 'ON TIME', 497, 221, 212, 503, 1841); 
+VALUES ('Los Angeles', 497, 62, 694, 117); 
 INSERT INTO two_airlines 
-VALUES ('ALASKA', 'DELAYED', 62, 12, 20, 102, 305); 
+VALUES ('Phoenix', 221, 12, 4840, 415); 
 INSERT INTO two_airlines 
-VALUES ('AM WEST', 'ON TIME', 694, 4840, 383, 320, 201); 
+VALUES ('San Diego', 212, 20, 383, 65); 
 INSERT INTO two_airlines 
-VALUES ('AM WEST', 'DELAYED', 117, 415, 65, 129, 61);
+VALUES('San Francisco', 503, 102, 320, 129); 
+INSERT INTO two_airlines 
+VALUES ('Seattle', 1841, 305, 201, 61); 
 
 SELECT * FROM two_airlines;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-SELECT * FROM two_airlines
